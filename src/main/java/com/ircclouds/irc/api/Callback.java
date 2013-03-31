15b38d71927714c65bf61dfc6b54464a233efaa7
miguel@lebane.se <miguel@lebane.se@63d6fa83-7ac6-0606-4eca-07a426b6025d>
2013-03-31 09:19:34 +0000

    more javadoc

--- src/main/java/com/ircclouds/irc/api/Callback.java
+++ src/main/java/com/ircclouds/irc/api/Callback.java
@@ -1,5 +1,12 @@
 package com.ircclouds.irc.api;
 
+/**
+ * A callback that returns T on success and {@link Exception} on failure
+ * 
+ * @author miguel@lebane.se
+ *
+ * @param <T>
+ */
 public interface Callback<T> extends ICallback<T, Exception>
 {
 	void onSuccess(T aObject);

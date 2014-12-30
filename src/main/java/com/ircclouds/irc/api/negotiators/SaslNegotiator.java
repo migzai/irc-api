64084d70950565ae2a5c550a22eb61dd323b2536
Danny van Heumen <danny@dannyvanheumen.nl>
2014-12-30 02:46:05 +0100

    SASL: Moved more intelligence into the individual states.
    States now communicate themselves. Not sure how to handle fail()
    messages: do nothing, abort, retry with preconstructed prepare message,
    ...? Plus some other outstanding TODOs/FIXMEs.

--- src/main/java/com/ircclouds/irc/api/negotiators/SaslNegotiator.java
+++ src/main/java/com/ircclouds/irc/api/negotiators/SaslNegotiator.java
@@ -42,10 +42,8 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 	private State state;
 	private IRCApi irc;
 
-	// TODO how to handle time-outs? (though not crucial since IRC server will also time-out)
-	// TODO move more intelligence into the state implementations to the point
-	// where negotiator only passes on information and does not make intelligent
-	// decisions anymore
+	// TODO How to handle time-outs? (though not crucial since IRC server will also time-out)
+	// TODO How to handle failure() messages? Retry everything once?
 	public SaslNegotiator(final String user, final String pass, final String authzid) {
 		if (user == null) {
 			throw new IllegalArgumentException("user cannot be null");
@@ -65,7 +63,7 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 			throw new IllegalArgumentException("irc instance is required");
 		}
 		this.irc = irc;
-		this.state = this.state.init();
+		this.state = this.state.init(irc);
 		return new CapReqCmd(SASL_CAPABILITY_ID);
 	}
 
@@ -81,22 +79,15 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 			// TODO implement CAP command builder with specialized message type
 			if (capAck.find() && saslAcknowledged(capAck.group(2))) {
 				// FIXME support both <nick> and * (but be more restrictive than the current "not-whitespace" requirement)
-				this.state = this.state.ack();
-				final StringBuilder reply = new StringBuilder(AUTHENTICATE);
-				for (String mechanism : this.state.mechanisms()) {
-					reply.append(mechanism).append(" ");
-				}
-				send(reply);
+				this.state = this.state.ack(capAck.group(1));
 			} else if (confirmation.find()) {
-				this.state = this.state.confirm(confirmation.group(1));
-				final String challenge = this.state.credentials(this.authzid, this.user, this.pass);
-				send(AUTHENTICATE + challenge);
+				this.state = this.state.confirm(confirmation.group(1), this.authzid, this.user, this.pass);
 			} else {
 				// IGNORING, currently ...
 			}
 		} catch (RuntimeException e) {
 			LOG.error("Error occurred during CAP negotiation. Prematurely ending CAP negotiation phase and continuing IRC registration as is.", e);
-			send(new CapEndCmd());
+			this.irc.rawMessage(new CapEndCmd().asString());
 		}
 	}
 
@@ -116,20 +107,12 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 			LOG.debug("SERVER: " + msg.asRaw());
 		}
 		try {
-			final int code = msg.getNumericCode();
-			final State previous = this.state;
-			switch (code) {
+			switch (msg.getNumericCode()) {
 				case RPL_LOGGEDIN:
 					this.state = this.state.loggedIn();
-					if (checkEndStateTransition(previous)) {
-						send(new CapEndCmd());
-					}
 					break;
 				case RPL_SASLSUCCESS:
 					this.state = this.state.success();
-					if (checkEndStateTransition(previous)) {
-						send(new CapEndCmd());
-					}
 					break;
 				case ERR_SASLFAIL:
 					this.state = this.state.fail();
@@ -137,21 +120,16 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 					break;
 				case ERR_SASLABORTED:
 					this.state = this.state.abort();
-					send(new CapEndCmd());
 					break;
 				case ERR_SASLALREADY:
-					// currently, sasl negotiation states do not have any kind of
-					// internal persistent state, so we can simply abort as it will
-					// bring us back to InitialState
 					this.state = this.state.abort();
-					send(new CapEndCmd());
 					break;
 				default:
 					break;
 			}
 		} catch (RuntimeException e) {
 			LOG.error("Error occurred during CAP negotiation. Ending CAP negotiation phase and continuing registration as is.", e);
-			send(new CapEndCmd());
+			this.irc.rawMessage(new CapEndCmd().asString());
 		}
 	}
 
@@ -161,40 +139,20 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 				&& this.state instanceof InitialState;
 	}
 
-	private void send(final ICommand cmd) {
-		send(cmd.asString());
-	}
-
-	private void send(final StringBuilder msg) {
-		send(msg.toString());
-	}
-
-	private void send(final String msg) {
-		if (LOG.isDebugEnabled()) {
-			LOG.debug("CLIENT: " + msg);
-		}
-		this.irc.rawMessage(msg);
-	}
-
 	private static abstract class State {
 		/**
 		 * Initialize sasl state now that CAP REQ sasl request has been sent.
 		 *
+		 * @param irc irc api instance
 		 * @return returns new state ready to commence sasl negotiation
 		 */
-		abstract State init();
+		abstract State init(IRCApi irc);
 		/**
 		 * Acknowledge that SASL negotiation is supported and can commence.
 		 *
 		 * @return returns new state
 		 */
-		abstract State ack();
-		/**
-		 * Query for supported mechanisms.
-		 *
-		 * @return returns array of supported mechanisms
-		 */
-		abstract String[] mechanisms();
+		abstract State ack(String user);
 		/**
 		 * Confirm proposed mechanism with optional max response length
 		 * parameter.
@@ -202,16 +160,7 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 		 * @param parameters optional max response length parameters
 		 * @return returns new state after processing confirmation
 		 */
-		abstract State confirm(String parameters);
-		/**
-		 * Generate credentials response based on provided data.
-		 *
-		 * @param authz authorization role
-		 * @param user user id
-		 * @param pass password
-		 * @return returns composed credentials response
-		 */
-		abstract String credentials(String authz, String user, String pass);
+		abstract State confirm(String parameters, String authzid, String user, String pass);
 		/**
 		 * Signal logged in.
 		 *
@@ -242,32 +191,46 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 		 * @return returns initial state after processing abort
 		 */
 		abstract State abort();
+
+		protected static void send(final IRCApi irc, final ICommand cmd) {
+			send(irc, cmd.asString());
+		}
+
+		protected static void send(final IRCApi irc, final String msg) {
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("CLIENT: " + msg);
+			}
+			irc.rawMessage(msg);
+		}
+
+		protected static String encode(final String authzid, final String user, final String pass) {
+			final StringBuilder response = new StringBuilder();
+			if (authzid != null) {
+				response.append(authzid);
+			}
+			response.append('\u0000').append(user).append('\u0000').append(pass);
+			try {
+				return Base64.encodeBase64String(response.toString().getBytes("UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				throw new IllegalArgumentException("Unsupported encoding specified.", e);
+			}
+		}
 	}
 
 	private static final class InitialState extends State {
 
 		@Override
-		State init() {
-			return new SaslRequested();
+		State init(final IRCApi irc) {
+			return new SaslRequested(irc);
 		}
 
 		@Override
-		State ack() {
+		State ack(final String user) {
 			throw new IllegalStateException("SASL not initialized.");
 		}
 
 		@Override
-		String[] mechanisms() {
-			throw new IllegalStateException("SASL not initialized.");
-		}
-
-		@Override
-		State confirm(final String parameters) {
-			throw new IllegalStateException("SASL not initialized.");
-		}
-
-		@Override
-		String credentials(final String authz, final String user, final String pass) {
+		State confirm(final String parameters, final String authzid, final String user, final String pass) {
 			throw new IllegalStateException("SASL not initialized.");
 		}
 
@@ -293,29 +256,29 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 	}
 
 	private static final class SaslRequested extends State {
+		private final IRCApi irc;
+
+		private SaslRequested(final IRCApi irc) {
+			if (irc == null) {
+				throw new IllegalArgumentException("irc cannot be null");
+			}
+			this.irc = irc;
+		}
 
 		@Override
-		State init() {
+		State init(final IRCApi irc) {
 			return this;
 		}
 
 		@Override
-		State ack() {
-			return new SaslAcknowledged();
+		State ack(final String user) {
+			// FIXME need to verify user?
+			send(this.irc, AUTHENTICATE + "PLAIN");
+			return new SaslAcknowledged(irc);
 		}
 
 		@Override
-		String[] mechanisms() {
-			throw new IllegalStateException("SASL not acknowledged. Awaiting acknowledgement of request.");
-		}
-
-		@Override
-		State confirm(final String parameters) {
-			throw new IllegalStateException("SASL not acknowledged. Awaiting acknowledgement of request.");
-		}
-
-		@Override
-		String credentials(final String authz, final String user, final String pass) {
+		State confirm(final String parameters, final String authzid, final String user, final String pass) {
 			throw new IllegalStateException("SASL not acknowledged. Awaiting acknowledgement of request.");
 		}
 
@@ -336,36 +299,36 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 
 		@Override
 		State abort() {
+			send(this.irc, new CapEndCmd());
 			return new InitialState();
 		}
 	}
 
 	private static final class SaslAcknowledged extends State {
+		private final IRCApi irc;
+
+		private SaslAcknowledged(final IRCApi irc) {
+			if (irc == null) {
+				throw new IllegalArgumentException("irc cannot be null");
+			}
+			this.irc = irc;
+		}
 
 		@Override
-		State init() {
+		State init(final IRCApi irc) {
 			throw new IllegalStateException("SASL already initialized. Awaiting acceptance of AUTHENTICATE proposal.");
 		}
 
 		@Override
-		State ack() {
+		State ack(final String user) {
 			// Not a big issue that we acknowledge twice, since we haven't gone further yet.
 			return this;
 		}
 
 		@Override
-		String[] mechanisms() {
-			return new String[] {"PLAIN"};
-		}
-
-		@Override
-		State confirm(final String parameters) {
-			return new SaslConfirmed();
-		}
-
-		@Override
-		String credentials(final String authz, final String user, final String pass) {
-			throw new IllegalStateException("SASL not confirmed yet. Awaiting acceptance of AUTHENTICATE proposal.");
+		State confirm(final String parameters, final String authzid, final String user, final String pass) {
+			send(this.irc, AUTHENTICATE + encode(authzid, user, pass));
+			return new SaslConfirmed(this.irc);
 		}
 
 		@Override
@@ -385,67 +348,59 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 
 		@Override
 		State abort() {
+			send(this.irc, new CapEndCmd());
 			return new InitialState();
 		}
 	}
 
 	private static final class SaslConfirmed extends State {
+		private final IRCApi irc;
 		private boolean loggedIn;
 		private boolean successful;
 
+		private SaslConfirmed(final IRCApi irc) {
+			if (irc == null) {
+				throw new IllegalArgumentException("irc cannot be null");
+			}
+			this.irc = irc;
+		}
+
 		@Override
-		State init() {
+		State init(final IRCApi irc) {
 			throw new IllegalStateException("SASL already initialized. Awaiting confirmation of successful log in.");
 		}
 
 		@Override
-		State ack() {
+		State ack(final String user) {
 			throw new IllegalStateException("SASL already acknowledged. Awaiting confirmation of successful log in.");
 		}
 
 		@Override
-		String[] mechanisms() {
-			throw new IllegalStateException("SASL already acknowledged. Awaiting confirmation of successful log in.");
-		}
-
-		@Override
-		State confirm(final String parameters) {
+		State confirm(final String parameters, final String authzid, final String user, final String pass) {
 			// FIXME store authentication requirement parameters
+			// FIXME resend challenge?
 			return this;
-		}
-
-		@Override
-		String credentials(final String authz, final String user, final String pass) {
-			return encode(authz, user, pass);
-		}
-
-		private String encode(final String authzid, final String user, final String pass) {
-			final StringBuilder response = new StringBuilder();
-			if (authzid != null) {
-				response.append(authzid);
-			}
-			response.append('\u0000').append(user).append('\u0000').append(pass);
-			try {
-				return Base64.encodeBase64String(response.toString().getBytes("UTF-8"));
-			} catch (UnsupportedEncodingException e) {
-				throw new IllegalArgumentException("Unsupported encoding specified.", e);
-			}
 		}
 
 		@Override
 		State loggedIn() {
 			this.loggedIn = true;
-			return next();
+			return process();
 		}
 
 		@Override
 		State success() {
 			this.successful = true;
-			return next();
+			return process();
 		}
 
-		private State next() {
-			return this.loggedIn && this.successful ? new InitialState() : this;
+		private State process() {
+			if (this.loggedIn && this.successful) {
+				send(this.irc, new CapEndCmd());
+				return new InitialState();
+			} else {
+				return this;
+			}
 		}
 
 		@Override
@@ -457,6 +412,7 @@ public class SaslNegotiator extends VariousMessageListenerAdapter implements Cap
 		@Override
 		State abort() {
 			reset();
+			send(this.irc, new CapEndCmd());
 			return new InitialState();
 		}
 

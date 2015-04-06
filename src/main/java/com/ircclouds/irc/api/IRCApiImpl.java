82b8d9d994eb1e7049232fd1a0e7f5a0a85874c0
Danny van Heumen <danny@dannyvanheumen.nl>
2015-04-06 15:04:49 +0200

    Added proxy support for DCC Receiver.
    Added proxy parameter for DCC receive and resume operations. Connection
    will be made through provided proxy instance. Or, if null instead of
    proxy instance provided, connection will not be proxied.

--- src/main/java/com/ircclouds/irc/api/IRCApiImpl.java
+++ src/main/java/com/ircclouds/irc/api/IRCApiImpl.java
@@ -427,9 +427,21 @@ public class IRCApiImpl implements IRCApi
 	}
 
 	@Override
+	public void dccReceive(File aFile, Integer aSize, SocketAddress aAddress, DCCReceiveCallback aCallback, Proxy aProxy)
+	{
+		dccResume(aFile, 0, aSize, aAddress, aCallback, aProxy);
+	}
+
+	@Override
 	public void dccResume(File aFile, Integer aResumePosition, Integer aSize, SocketAddress aAddress, DCCReceiveCallback aCallback)
 	{
 		dccManager.dccResume(aFile, aResumePosition, aSize, aAddress, aCallback);
+	}
+
+	@Override
+	public void dccResume(File aFile, Integer aResumePosition, Integer aSize, SocketAddress aAddress, DCCReceiveCallback aCallback, Proxy aProxy)
+	{
+		dccManager.dccResume(aFile, aResumePosition, aSize, aAddress, aCallback, aProxy);
 	}
 
 	@Override

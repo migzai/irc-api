165803c196c2d27cbd3953536e39028b51520a7f
Danny van Heumen <danny@dannyvanheumen.nl>
2015-04-06 20:40:07 +0200

    Also signal exception to api in case of End of Stream, so there is feedback of connection failure in case of connection cut off during IRC registration process.

--- src/main/java/com/ircclouds/irc/api/AbstractApiDaemon.java
+++ src/main/java/com/ircclouds/irc/api/AbstractApiDaemon.java
@@ -13,8 +13,8 @@ public abstract class AbstractApiDaemon extends Thread
 {
 	private static final Logger LOG = LoggerFactory.getLogger(AbstractApiDaemon.class);
 
-	private IMessageReader reader;
-	private IMessageDispatcher dispatcher;
+	private final IMessageReader reader;
+	private final IMessageDispatcher dispatcher;
 
 	public AbstractApiDaemon(IMessageReader aReader, IMessageDispatcher aDispatcher)
 	{
@@ -24,6 +24,7 @@ public abstract class AbstractApiDaemon extends Thread
 		dispatcher = aDispatcher;
 	}
 
+	@Override
 	public void run()
 	{
 		try
@@ -53,11 +54,16 @@ public abstract class AbstractApiDaemon extends Thread
 		catch (EndOfStreamException aExc)
 		{
 			LOG.debug("Received end of stream, closing connection", aExc);
+			// Signaling the exception to the api is necessary, since there is a
+			// chance that the connection is abruptly cut off before connection
+			// (or rather IRC registration process) is finished. Otherwise,
+			// there would be no feedback of the connection failure.
+			signalExceptionToApi(aExc);
+			dispatcher.dispatch(new ClientErrorMessage(aExc), TargetListeners.ALL);
 		}
 		catch (IOException aExc)
 		{
 			LOG.error(this.getName(), aExc);
-
 			signalExceptionToApi(aExc);
 			dispatcher.dispatch(new ClientErrorMessage(aExc), TargetListeners.ALL);
 		}
